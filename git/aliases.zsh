9d4dfc88efe58f0ac0347b74e0d79109b9bceab4
timraasveld <ttimitri@gmail.com>
2017-02-14 11:39:37 +0100

    Add Git functions to temporarily use branch, stashing all current work

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -6,7 +6,6 @@ then
   alias git=$hub_path
 fi
 
-# The rest of my fun git aliases
 alias gl='git pull --prune'
 alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
 alias gp='git push origin HEAD'
@@ -17,3 +16,21 @@ alias gco='git checkout'
 alias gcb='git copy-branch-name'
 alias gb='git branch'
 alias gs='git status -sb' # upgrade your git if -sb breaks for you. it's fun.
+
+tub(){ # Temporarily use branch, default master
+  set -o xtrace
+  branch_name=$(git symbolic-ref -q HEAD)
+  branch_name=${branch_name##refs/heads/}
+  branch_name=${branch_name:-HEAD}
+  export LAST_USED_BRANCH=$branch_name
+
+  git add -A
+  git stash
+  git checkout ${1:-master}
+}
+
+cw() { # Continue working (on work stashed using tub function)
+  set -o xtrace
+  git checkout $LAST_USED_BRANCH
+  git stash pop
+}
