f6f733246dbe269c84a6cfe59ec9bda0cdd2a501
timraasveld <ttimitri@gmail.com>
2017-02-17 10:31:50 +0100

    Add git-reflow workaround

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -3,3 +3,5 @@ alias reload!='. ~/.zshrc'
 alias l='tail -f -n 100 log/development.log'
 
 alias be='bundle exec'
+
+alias git-reflow='rvm ruby-2.2.0 do git-reflow' # Fix for "already activated" bundler bug
