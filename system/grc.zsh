5c5d80fc9db30679a2ba45b92c1f25ad0dad0604
timraasveld <ttimitri@gmail.com>
2017-02-17 10:09:59 +0100

    Fix sed command

--- system/grc.zsh
+++ system/grc.zsh
@@ -1,6 +1,6 @@
 # GRC colorizes nifty unix tools all over the place
 if (( $+commands[grc] )) && (( $+commands[brew] ))
 then
-  sed -i 's/type -p grc/which grc/g' /usr/local/etc/grc.bashrc # type -p returns extra output on OSX, breaking the script. Use which instead.
+  sed -i -e 's/type -p grc/which grc/g' /usr/local/etc/grc.bashrc # type -p returns extra output on OSX, breaking the script. Use which instead.
   source `brew --prefix`/etc/grc.bashrc
 fi
