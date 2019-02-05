52db43895de73782036b0afc5a4503996d283f51
Tim Raasveld <ttimitri@gmail.com>
2019-02-05 16:37:05 +0100

    Add git add alias

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -9,6 +9,7 @@ fi
 alias gl='git pull --prune'
 alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
 alias gp='git push origin HEAD'
+alias ga='git add'
 alias gd='git diff'
 alias gc='git commit'
 alias gca='git commit -a'
